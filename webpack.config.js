dff1213644405e237b518aef2a702eae6144d2b9
scode234 <scodes3451@gmail.com>
2022-05-19 19:14:18 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    watch: true,
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gift)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf)$/,
+                type: 'asset/resource',
+            },
+        ],
+    },
+}
