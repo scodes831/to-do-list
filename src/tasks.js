b7e9bebb3d4347350edbcf1a0f0a2fc84929d712
scode234 <scodes3451@gmail.com>
2022-05-21 16:12:23 -0500

    Remove divs on click and generate new task form

---
+++ src/tasks.js
@@ -0,0 +1,73 @@
+const container = document.getElementById('content-container');
+const newTaskDiv = document.getElementById('new-task');
+const newProjectDiv = document.getElementById('new-project');
+
+export function removeNewDivs() {
+    newTaskDiv.style.display = "none";
+    newProjectDiv.style.display = "none"
+}
+
+export function addTaskForm() {
+    const taskFormContainer = document.createElement('div');
+    taskFormContainer.setAttribute('id', 'task-form-container');
+    container.appendChild(taskFormContainer);
+
+    const taskForm = document.createElement('form');
+    taskForm.setAttribute('id', 'task-form');
+    taskFormContainer.appendChild(taskForm);
+
+    const taskFormTitle = document.createElement('h2');
+    taskFormTitle.classList.add('form-title');
+    taskForm.appendChild(taskFormTitle);
+    taskFormTitle.textContent = "Add New Task";
+
+    const taskFormNameLabel = document.createElement('label');
+    taskFormNameLabel.setAttribute('for', 'task-name');
+    taskFormNameLabel.textContent = "Name:";
+    taskForm.appendChild(taskFormNameLabel);
+
+    const taskFormNameInput = document.createElement('input');
+    taskFormNameInput.setAttribute('type', 'text');
+    taskFormNameInput.setAttribute('name', 'task-name');
+    taskForm.appendChild(taskFormNameInput);
+
+    const taskFormDatelabel = document.createElement('label');
+    taskFormNameLabel.setAttribute('for', 'task-date');
+    taskFormDatelabel.textContent = "Due Date:";
+    taskForm.appendChild(taskFormDatelabel);
+
+    const taskFormDateInput = document.createElement('input');
+    taskFormDateInput.setAttribute('type', 'date');
+    taskFormDateInput.setAttribute('name', 'task-date');
+    taskForm.appendChild(taskFormDateInput);
+
+    const taskFormDescLabel = document.createElement('label');
+    taskFormDescLabel.setAttribute('for', 'task-desc');
+    taskFormDescLabel.textContent = "Description:";
+    taskForm.appendChild(taskFormDescLabel);
+
+    const taskFormDescInput = document.createElement('input');
+    taskFormDescInput.setAttribute('type', 'text');
+    taskFormDescInput.setAttribute('name', 'task-desc');
+    taskForm.appendChild(taskFormDescInput);
+
+    const taskFormPriorityLabel = document.createElement('label');
+    taskFormPriorityLabel.setAttribute('for', 'task-priority');
+    taskFormPriorityLabel.textContent = "Priority:";
+    taskForm.appendChild(taskFormPriorityLabel);
+
+    const taskFormPriorityInput = document.createElement('select');
+    taskFormPriorityInput.setAttribute('name', 'task-priority');
+    taskForm.appendChild(taskFormPriorityInput);
+
+    const taskFormProjectLabel = document.createElement('label')
+    taskFormProjectLabel.setAttribute('for', 'task-project');
+    taskFormProjectLabel.textContent = "Associated with Project:";
+    taskForm.appendChild(taskFormProjectLabel);
+
+    const taskFormProjectInput = document.createElement('select');
+    taskFormProjectInput.setAttribute('name', 'task-project');
+    taskForm.appendChild(taskFormProjectInput);
+
+}
+
