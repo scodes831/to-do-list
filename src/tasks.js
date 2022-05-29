bf14c24d396d16f57454beaee766b292b51c9372
scode234 <scodes3451@gmail.com>
2022-05-29 18:24:36 -0500

    Update task numbers in nav bar dynamically as tasks are submitted

--- src/tasks.js
+++ src/tasks.js
@@ -3,6 +3,8 @@ import lowPriorityImage from './images/icons/low.png';
 import medPriorityImage from './images/icons/medium.png';
 import highPriorityImage from './images/icons/high.png';
 
+import { compareAsc, isSameMonth, isSameWeek } from 'date-fns';
+
 const tasksContainer = document.createElement('div');
 tasksContainer.setAttribute('id', 'tasks-card-container');
 
@@ -20,6 +22,8 @@ class Task {
     }
 
     makeCard(tName, tDate, tDesc, tPriority, tProj) {
+
+        console.log(tName, tDate, tDesc, tPriority, tProj);
         let card = document.createElement('div');
         card.classList.add('task-card');
         tasksContainer.appendChild(card);
@@ -62,33 +66,21 @@ class Task {
         taskCompleteLabel.textContent = "Completed:"
         taskCompleteLabel.setAttribute('for', 'task-checkbox');
         taskCompleteLabel.classList.add('task-completed-label');
-        card.appendChild(taskComplete);
+        card.appendChild(taskCompleteLabel);
 
         let taskComplete = document.createElement('input');
         taskComplete.setAttribute('type', 'checkbox');
         taskComplete.setAttribute('name', 'task-checkbox');
         taskComplete.classList.add('task-completed-box');
         card.appendChild(taskComplete);
+
+        console.log("task is now displayed");
     }
 }
 
-class TaskList {
-    constructor() {
-        this.taskList = []
-    }
+let taskList = [];
+let numAllTasks = 0;
 
-    newTask(name, dueDate, desc, priority, assocProj) {
-        let task = new Task(name, dueDate, desc, priority, assocProj)
-        this.taskList.push(task);
-        return task;
-    }
-
-    get allTasks() {
-        return this.taskList
-    }
-}
-
-let tasks = new TaskList();
 
 export function saveTask() {
     console.log("test test test");
@@ -103,29 +95,27 @@ export function saveTask() {
     console.log("task priority is " + taskPriority);
     let taskProject = document.getElementById('task-project').value;
     console.log("task project is " + taskProject);
-
     
     createTask();
 
     function createTask() {
+        const displayNumAllTasks = document.getElementById('num-all-tasks');
+
         if (taskName === "" || taskDate === "") {
             alert("You must enter a name and due date for the task.")
         } else {
-            tasks.newTask(taskName, taskDate, taskDesc, taskPriority, taskProject);
-            console.log(tasks.allTasks);
+            let task = new Task(taskName, taskDate, taskDesc, taskPriority, taskProject);
+            taskList.push(task);
+            console.log(taskList);
             displaySuccessfulAlert();
+            let addTasks = ++numAllTasks;
+            displayNumAllTasks.textContent = addTasks;
+            displayTasks(task);
             clearForm();
             setTimeout(closeSuccessfulAlert, 1500);
-            displayTask(taskName, taskDate, taskDate, taskPriority, taskProject)
         }
-
-    }
-
-    function displayTask() {
-        tasks.allTasks.forEach(task => {
-            task.makeCard();
-        })
-    }
+        
+    } 
 }
 
 function displaySuccessfulAlert() {
@@ -160,11 +150,95 @@ export function clearForm() {
     })
 }
 
-function displayTasks() {
-    let tasks = new TaskList();
-    tasks.allTasks.forEach(task => {
-        task.makeCard();
-    })
+let numDueToday = 0;
+let numDueThisWeek = 0;
+let numDueThisMonth = 0;
+
+function displayTasks(taskInput) {
+    console.log(taskInput);
+
+    const displayNumDueToday = document.getElementById('num-due-today');
+    const displayNumDueThisWeek = document.getElementById('num-due-this-week');
+    const displayNumDueThisMonth = document.getElementById('num-due-this-month');
+
+    let today = new Date();
+    let taskDate = taskInput.dueDate;
+    let newTaskDate = taskDate.split('-');
+    let dueDay = +newTaskDate[2];
+    // console.log("day due is " + dueDay);
+    let dueMonth = newTaskDate[1]-1;
+    // console.log("month due is " + dueMonth);
+    let dueYear = +newTaskDate[0];
+    // console.log("year due is " + dueYear);
+    let todayDay = +today.getDate();
+    // console.log("day today is " + todayDay);
+    let todayMonth = +today.getMonth();
+    // console.log("this month is " + todayMonth);
+    let todayYear = +today.getFullYear();
+    // console.log("this year is " + todayYear);
+    today = new Date(todayYear, todayMonth, todayDay);
+    taskDate = new Date(dueYear, dueMonth, dueDay);
+            
+    addToTodayList(today, taskDate, displayNumDueToday, taskInput);
+    addToThisWeekList(today, taskDate, displayNumDueThisWeek, taskInput);
+    addToThisMonthList(today, taskDate, displayNumDueThisMonth, taskInput);
+}
+
+let todayList = [];
+let thisWeekList = [];
+let thisMonthList = [];
+
+function addToTodayList(dateA, dateB, display, taskInput) {
+    if (compareAsc(dateA, dateB) === 0) {
+        console.log("it's due today");
+        let addToday = ++numDueToday;
+        console.log("add is currently " + addToday);
+        display.textContent = addToday;
+        todayList.push(taskInput)
+        console.log(todayList);
+    }
+};
+
+export function displayTodayList() {
+    createTaskDiv();
+
+};
+
+function addToThisWeekList(dateA, dateB, display, taskInput) {
+    if (isSameWeek(dateA, dateB)) {
+        console.log("it's due this week");
+        let addWeek = ++numDueThisWeek;
+        display.textContent = addWeek;
+        thisWeekList.push(taskInput);
+        console.log(thisWeekList);
+    }
+};
+
+export function displayThisWeekList() {
+    createTaskDiv();
+
+};
+
+function addToThisMonthList(dateA, dateB, display, taskInput) {
+    if (isSameMonth(dateA, dateB)) {
+        console.log("it's due this month");
+        let addMonth = ++numDueThisMonth;
+        display.textContent = addMonth;
+        thisMonthList.push(taskInput);
+        console.log(thisMonthList);
+    }
+};
+
+export function displayThisMonthList() {
+    createTaskDiv();
+
+};
+
+function createTaskDiv() {
+    const container = document.getElementById('content-container');
+    const newTaskContainer = document.createElement('div');
+    newTaskContainer.setAttribute('id', 'new-task-container');
+    container.appendChild(newTaskContainer);
 }
 
 
