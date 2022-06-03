6c06166b1978db90aecb3e7edac6f0b7d5174729
scode234 <scodes3451@gmail.com>
2022-06-02 20:26:07 -0500

    Remove task table when clicking on nav link

--- src/index.js
+++ src/index.js
@@ -1,12 +1,309 @@
 require('./style.css');
 
+import { saveProject } from './projects';
+import { saveTask, 
+        clearForm, 
+        removeTaskDiv,
+        removeTaskTable,
+        displayAllTasksList,
+        displayThisMonthList, 
+        displayThisWeekList, 
+        displayTodayList, } from './tasks';
+
 const addTaskBtn = document.getElementById('add-task');
 const addProjectBtn = document.getElementById('add-project');
+const container = document.getElementById('content-container');
+const newTaskDiv = document.getElementById('new-task');
+const newProjectDiv = document.getElementById('new-project');
+const navLinks = document.querySelectorAll('.nav-links');
+const allTasks = document.getElementById('all-tasks');
+const myProjects = document.getElementById('my-projects');
+const homeLink = document.querySelector('.home');
+const taskFormContainer = document.getElementById('task-form-container');
+const projectFormContainer = document.getElementById('project-form-container');
+const tasksTableContainer = document.getElementById('new-task-container');
+const taskDeleteIcons = document.querySelectorAll('.delete-task');
 
-addTaskBtn.addEventListener('click', e => {
-    console.log("task added");
+let userClicks = ["home"];
+
+addTaskBtn.addEventListener('click', () => {
+    removeNewDivs();
+    addTaskForm();
 });
 
-addProjectBtn.addEventListener('click', e=> {
-    console.log("project added");
-})
+addProjectBtn.addEventListener('click', () => {
+    removeNewDivs();
+    addProjectForm();
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', e => {
+        let nextPage = e.target.id;
+        userClicks.push(nextPage);
+        let previousPage = userClicks[(userClicks.length-2)];
+        console.log("the next page is " + nextPage);
+        console.log("the previous page was " + previousPage);
+        console.log(userClicks);
+        if (previousPage === "home") {
+            removeTaskFormContainer();
+            removeNewDivs();
+        } else if (previousPage === "due-today" || previousPage === "due-this-week" || previousPage === "due-this-month") {
+            removeTaskFormContainer();
+            removeTaskTable();
+        }       
+
+        if (e.target.id === "due-today") {
+            console.log("display tasks due today");
+            displayTodayList();
+        } else if (e.target.id === "due-this-week") {
+            console.log("display tasks due this week");
+            displayThisWeekList();
+        } else if (e.target.id === "due-this-month") {
+            console.log("display tasks due this month");
+            displayThisMonthList();
+        } else if (e.target.id === "all-tasks") {
+            displayAllTasksList();
+        } else if (e.target.id === "my-projects") {
+            displayMyProjectsList();
+        } else if (e.target.id === "home") {
+            displayNewDivs();
+        }
+    });
+});
+
+taskDeleteIcons.forEach(icon => {
+    icon.addEventListener('click', e => {
+        console.log("the task is deleted");
+    })
+});
+
+function removeNewDivs() {
+    newTaskDiv.style.display = "none";
+    newProjectDiv.style.display = "none";
+};
+
+function displayNewDivs() {
+    newTaskDiv.style.display = "flex";
+    newProjectDiv.style.display = "flex";
+};
+
+function removeTaskFormContainer() {
+    const taskFormContainer = document.getElementById('task-form-container');
+    if (taskFormContainer.classList.contains('active')) {
+        taskFormContainer.remove();
+        taskFormContainer.classList.remove('active');
+    } else {
+        return;
+    }    
+};
+
+function displayTaskFormContainer() {
+    taskFormContainer.style.display = "block";
+};
+
+function removeProjectFormContainer() {
+    projectFormContainer.style.display = "none";
+}
+
+function displayProjectFormContainer() {
+    projectFormContainer.style.display = "block";
+
+}
+
+function addProjectForm() {
+    const projectFormContainer = document.createElement('div');
+    projectFormContainer.setAttribute('id', 'project-form-container');
+    projectFormContainer.classList.add('active');
+    container.appendChild(projectFormContainer);
+
+    const projectForm = document.createElement('form');
+    projectForm.setAttribute('id', 'project-form');
+    projectFormContainer.appendChild(projectForm);
+
+    const projectFormTitle = document.createElement('h2');
+    projectFormTitle.classList.add('form-title');
+    projectFormTitle.textContent = "Add New Project";
+    projectForm.appendChild(projectFormTitle);
+
+    const projectFormNameLabel = document.createElement('label');
+    projectFormNameLabel.setAttribute('for', 'project-name');
+    projectFormNameLabel.textContent = "Name:";
+    projectForm.appendChild(projectFormNameLabel);
+
+    const projectFormNameInput = document.createElement('input');
+    projectFormNameInput.setAttribute('id', 'project-name');
+    projectFormNameInput.setAttribute('type', 'text');
+    projectFormNameInput.setAttribute('name', 'project-name');
+    projectForm.appendChild(projectFormNameInput);
+
+    const projectFormDeadlineLabel = document.createElement('label');
+    projectFormDeadlineLabel.setAttribute('for', 'project-deadline');
+    projectFormDeadlineLabel.textContent = "Project Deadline:";
+    projectForm.appendChild(projectFormDeadlineLabel);
+
+
+    const projectFormDeadlineInput = document.createElement('input');
+    projectFormDeadlineInput.setAttribute('id', 'project-deadline');
+    projectFormDeadlineInput.setAttribute('type', 'date');
+    projectFormDeadlineInput.setAttribute('name', 'project-deadline');
+    projectForm.appendChild(projectFormDeadlineInput);
+
+    const projectFormDescLabel = document.createElement('label');
+    projectFormDescLabel.setAttribute('for', 'project-desc');
+    projectFormDescLabel.textContent = "Description:";
+    projectForm.appendChild(projectFormDescLabel);
+
+    const projectFormDescInput = document.createElement('input');
+    projectFormDescInput.setAttribute('id', 'project-desc');
+    projectFormDescInput.setAttribute('type', 'text');
+    projectFormDescInput.setAttribute('name', 'project-desc');
+    projectForm.appendChild(projectFormDescInput);
+
+    const projectBtnContainer = document.createElement('div');
+    projectBtnContainer.setAttribute('id', 'project-button-container');
+    projectForm.appendChild(projectBtnContainer);
+
+    const projectSubmitBtn = document.createElement('button');
+    projectSubmitBtn.setAttribute('id', 'submit-project');
+    projectSubmitBtn.setAttribute('type', 'button');
+    projectSubmitBtn.textContent = "Submit";
+    projectBtnContainer.appendChild(projectSubmitBtn);
+    projectSubmitBtn.addEventListener('click', () => {
+        console.log('submit clicked');
+        saveProject();
+    });
+
+    const projectCancelBtn = document.createElement('button');
+    projectCancelBtn.setAttribute('id', 'cancel-project');
+    projectCancelBtn.setAttribute('type', 'button');
+    projectCancelBtn.textContent = "Cancel";
+    projectBtnContainer.appendChild(projectCancelBtn);
+    projectCancelBtn.addEventListener('click', () => {
+        removeTaskForm(projectFormContainer);
+        displayNewDivs();
+    })
+}
+
+function addTaskForm() {
+    const taskFormContainer = document.createElement('div');
+    taskFormContainer.setAttribute('id', 'task-form-container');
+    taskFormContainer.classList.add('active');
+    container.appendChild(taskFormContainer);
+
+    const taskForm = document.createElement('form');
+    taskForm.setAttribute('id', 'task-form');
+    taskFormContainer.appendChild(taskForm);
+
+    const taskFormTitle = document.createElement('h2');
+    taskFormTitle.classList.add('form-title');
+    taskForm.appendChild(taskFormTitle);
+    taskFormTitle.textContent = "Add New Task";
+
+    const taskFormNameLabel = document.createElement('label');
+    taskFormNameLabel.setAttribute('for', 'task-name');
+    taskFormNameLabel.textContent = "Name:";
+    taskForm.appendChild(taskFormNameLabel);
+
+    const taskFormNameInput = document.createElement('input');
+    taskFormNameInput.setAttribute('id', 'task-name');
+    taskFormNameInput.setAttribute('type', 'text');
+    taskFormNameInput.setAttribute('name', 'task-name');
+    taskForm.appendChild(taskFormNameInput);
+
+    const taskFormDatelabel = document.createElement('label');
+    taskFormDatelabel.setAttribute('for', 'task-date');
+    taskFormDatelabel.textContent = "Due Date:";
+    taskForm.appendChild(taskFormDatelabel);
+
+    const taskFormDateInput = document.createElement('input');
+    taskFormDateInput.setAttribute('id', 'task-date');
+    taskFormDateInput.setAttribute('type', 'date');
+    taskFormDateInput.setAttribute('name', 'task-date');
+    taskForm.appendChild(taskFormDateInput);
+
+    const taskFormDescLabel = document.createElement('label');
+    taskFormDescLabel.setAttribute('for', 'task-desc');
+    taskFormDescLabel.textContent = "Description:";
+    taskForm.appendChild(taskFormDescLabel);
+
+    const taskFormDescInput = document.createElement('input');
+    taskFormDescInput.setAttribute('id', 'task-desc');
+    taskFormDescInput.setAttribute('type', 'text');
+    taskFormDescInput.setAttribute('name', 'task-desc');
+    taskForm.appendChild(taskFormDescInput);
+
+    const taskFormPriorityLabel = document.createElement('label');
+    taskFormPriorityLabel.setAttribute('for', 'task-priority');
+    taskFormPriorityLabel.textContent = "Priority:";
+    taskForm.appendChild(taskFormPriorityLabel);
+
+    const taskFormPriorityInput = document.createElement('select');
+    taskFormPriorityInput.setAttribute('id', 'task-priority');
+    taskFormPriorityInput.setAttribute('name', 'task-priority');
+    taskForm.appendChild(taskFormPriorityInput);
+
+    const taskFormPriorityLow = document.createElement('option');
+    taskFormPriorityLow.setAttribute('value', 'low');
+    taskFormPriorityLow.textContent = "Low";
+    taskFormPriorityInput.appendChild(taskFormPriorityLow);
+
+    const taskFormPriorityMed = document.createElement('option');
+    taskFormPriorityMed.setAttribute('value', 'medium');
+    taskFormPriorityMed.textContent = "Medium";
+    taskFormPriorityInput.appendChild(taskFormPriorityMed);
+
+    const taskFormPriorityHigh = document.createElement('option');
+    taskFormPriorityHigh.setAttribute('value', 'high');
+    taskFormPriorityHigh.textContent = "High";
+    taskFormPriorityInput.appendChild(taskFormPriorityHigh);
+
+    const taskFormProjectLabel = document.createElement('label')
+    taskFormProjectLabel.setAttribute('for', 'task-project');
+    taskFormProjectLabel.textContent = "Associated with Project:";
+    taskForm.appendChild(taskFormProjectLabel);
+
+    const taskFormProjectInput = document.createElement('select');
+    taskFormProjectInput.setAttribute('id', 'task-project');
+    taskFormProjectInput.setAttribute('name', 'task-project');
+    taskForm.appendChild(taskFormProjectInput);
+
+    const taskFormProjectTest = document.createElement('option');
+    taskFormProjectTest.setAttribute('value', 'test-project');
+    taskFormProjectTest.textContent = "Test Project";
+    taskFormProjectInput.appendChild(taskFormProjectTest);
+
+    const taskBtnContainer = document.createElement('div');
+    taskBtnContainer.setAttribute('id', 'task-button-container');
+    taskForm.appendChild(taskBtnContainer);
+
+    const taskSubmitBtn = document.createElement('button');
+    taskSubmitBtn.setAttribute('id', 'submit-task');
+    taskSubmitBtn.setAttribute('type', 'button');
+    taskSubmitBtn.textContent = "Submit";
+    taskBtnContainer.appendChild(taskSubmitBtn);
+    taskSubmitBtn.addEventListener('click', () => {
+        console.log('submit clicked');
+        saveTask();
+    });
+
+    const taskCancelBtn = document.createElement('button');
+    taskCancelBtn.setAttribute('id', 'cancel-task');
+    taskCancelBtn.setAttribute('type', 'button');
+    taskCancelBtn.textContent = "Cancel";
+    taskBtnContainer.appendChild(taskCancelBtn);
+    taskCancelBtn.addEventListener('click', () => {
+        removeTaskForm(taskFormContainer);
+        displayNewDivs();
+        console.log("it worked");
+    });
+};
+
+function removeTaskForm(container) {
+    clearForm();
+    container.style.display = "none";
+};
+
+
+
+
+
--- src/tasks.js
+++ src/tasks.js
@@ -1,4 +1,5 @@
 import greenCheckmarkImage from './images/icons/green-checkmark.png';
+import trashBinImage from './images/icons/trash-bin.png';
 import lowPriorityImage from './images/icons/low.png';
 import medPriorityImage from './images/icons/medium.png';
 import highPriorityImage from './images/icons/high.png';
@@ -20,62 +21,6 @@ class Task {
     showTask() {
         console.log(this.name, this.dueDate, this.desc, this.priority, this.assocProj)
     }
-
-    makeCard(tName, tDate, tDesc, tPriority, tProj) {
-
-        console.log(tName, tDate, tDesc, tPriority, tProj);
-        let card = document.createElement('div');
-        card.classList.add('task-card');
-        tasksContainer.appendChild(card);
-
-        let name = document.createElement('h2');
-        name.classList.add('card-task-name');
-        name.textContent = tName;
-        card.appendChild(name);
-
-        let dueDate = document.createElement('h2');
-        dueDate.classList.add('card-task-date');
-        dueDate.textContent = tDate;
-        card.appendChild(dueDate);
-
-        let desc = document.createElement('p');
-        desc.classList.add('card-task-desc');
-        desc.textContent = tDesc;
-        card.appendChild(desc);
-
-        let priority = document.createElement('img');
-        priority.classList.add('card-task-priority');
-        if (tPriority === "low") {
-            priority.src = lowPriorityImage;
-            priority.alt = "low priority"
-        } else if (tPriority === "medium") {
-            priority.src = medPriorityImage;
-            priority.alt = "medium priority";
-        } else if (tPriority === "high") {
-            priority.src = highPriorityImage;
-            priority.alt = "high priority";
-        }
-        card.appendChild(priority);
-
-        let assocProj = document.createElement('h3');
-        assocProj.classList.add('card-task-project');
-        assocProj.textContent = tProj;
-        card.appendChild(assocProj);
-
-        let taskCompleteLabel = document.createElement('label');
-        taskCompleteLabel.textContent = "Completed:"
-        taskCompleteLabel.setAttribute('for', 'task-checkbox');
-        taskCompleteLabel.classList.add('task-completed-label');
-        card.appendChild(taskCompleteLabel);
-
-        let taskComplete = document.createElement('input');
-        taskComplete.setAttribute('type', 'checkbox');
-        taskComplete.setAttribute('name', 'task-checkbox');
-        taskComplete.classList.add('task-completed-box');
-        card.appendChild(taskComplete);
-
-        console.log("task is now displayed");
-    }
 }
 
 let taskList = [];
@@ -86,15 +31,10 @@ export function saveTask() {
     console.log("test test test");
 
     let taskName = document.getElementById('task-name').value;
-    console.log("task name is " + taskName);
     let taskDate = document.getElementById('task-date').value;
-    console.log("task date is " + taskDate);
     let taskDesc = document.getElementById('task-desc').value;
-    console.log("task desc is " + taskDesc);
     let taskPriority = document.getElementById('task-priority').value;
-    console.log("task priority is " + taskPriority);
     let taskProject = document.getElementById('task-project').value;
-    console.log("task project is " + taskProject);
     
     createTask();
 
@@ -137,7 +77,7 @@ function displaySuccessfulAlert() {
     modalContainer.appendChild(modalIcon);
 }
 
-function closeSuccessfulAlert() {
+export function closeSuccessfulAlert() {
     const modal = document.getElementById('modal-container');
     modal.parentElement.removeChild(modal);
 }
@@ -148,7 +88,7 @@ export function clearForm() {
         console.log("cleared");
         input.value = "";
     })
-}
+};
 
 let numDueToday = 0;
 let numDueThisWeek = 0;
@@ -165,24 +105,18 @@ function displayTasks(taskInput) {
     let taskDate = taskInput.dueDate;
     let newTaskDate = taskDate.split('-');
     let dueDay = +newTaskDate[2];
-    // console.log("day due is " + dueDay);
     let dueMonth = newTaskDate[1]-1;
-    // console.log("month due is " + dueMonth);
     let dueYear = +newTaskDate[0];
-    // console.log("year due is " + dueYear);
     let todayDay = +today.getDate();
-    // console.log("day today is " + todayDay);
     let todayMonth = +today.getMonth();
-    // console.log("this month is " + todayMonth);
     let todayYear = +today.getFullYear();
-    // console.log("this year is " + todayYear);
     today = new Date(todayYear, todayMonth, todayDay);
     taskDate = new Date(dueYear, dueMonth, dueDay);
             
     addToTodayList(today, taskDate, displayNumDueToday, taskInput);
     addToThisWeekList(today, taskDate, displayNumDueThisWeek, taskInput);
     addToThisMonthList(today, taskDate, displayNumDueThisMonth, taskInput);
-}
+};
 
 let todayList = [];
 let thisWeekList = [];
@@ -190,23 +124,22 @@ let thisMonthList = [];
 
 function addToTodayList(dateA, dateB, display, taskInput) {
     if (compareAsc(dateA, dateB) === 0) {
-        console.log("it's due today");
+        // console.log("it's due today");
         let addToday = ++numDueToday;
-        console.log("add is currently " + addToday);
+        // console.log("add is currently " + addToday);
         display.textContent = addToday;
         todayList.push(taskInput)
         console.log(todayList);
-    }
+    } 
 };
 
 export function displayTodayList() {
-    createTaskDiv();
-
+    createTaskTable(todayList);
 };
 
 function addToThisWeekList(dateA, dateB, display, taskInput) {
     if (isSameWeek(dateA, dateB)) {
-        console.log("it's due this week");
+        // console.log("it's due this week");
         let addWeek = ++numDueThisWeek;
         display.textContent = addWeek;
         thisWeekList.push(taskInput);
@@ -215,13 +148,12 @@ function addToThisWeekList(dateA, dateB, display, taskInput) {
 };
 
 export function displayThisWeekList() {
-    createTaskDiv();
-
+    createTaskTable(thisWeekList);
 };
 
 function addToThisMonthList(dateA, dateB, display, taskInput) {
     if (isSameMonth(dateA, dateB)) {
-        console.log("it's due this month");
+        // console.log("it's due this month");
         let addMonth = ++numDueThisMonth;
         display.textContent = addMonth;
         thisMonthList.push(taskInput);
@@ -230,16 +162,130 @@ function addToThisMonthList(dateA, dateB, display, taskInput) {
 };
 
 export function displayThisMonthList() {
-    createTaskDiv();
-
+    createTaskTable(thisMonthList);
 };
 
-function createTaskDiv() {
-    const container = document.getElementById('content-container');
-    const newTaskContainer = document.createElement('div');
-    newTaskContainer.setAttribute('id', 'new-task-container');
-    container.appendChild(newTaskContainer);
+export function displayAllTasksList() {
+    console.log("displaying all tasks");
+    createTaskTable(taskList);
 }
+
+function createTaskTable(a) {
+    const container = document.getElementById('content-container');
+    const tasksTableContainer = document.createElement('div');
+    tasksTableContainer.setAttribute('id', 'new-task-container');
+    tasksTableContainer.classList.add('active');
+    container.appendChild(tasksTableContainer);
+
+    const tasksTable = document.createElement('table');
+    tasksTable.setAttribute('id', 'tasks-table');
+    tasksTableContainer.appendChild(tasksTable);
+
+    const tableHead = document.createElement('thead');
+    tasksTable.appendChild(tableHead);
+
+    const headRow = document.createElement('tr');
+    tableHead.appendChild(headRow);
+
+    const completedHeading = document.createElement('th');
+    headRow.appendChild(completedHeading);
+
+    const dateHeading = document.createElement('th');
+    dateHeading.classList.add('heading');
+    dateHeading.textContent = "Due Date";
+    headRow.appendChild(dateHeading);
+
+    const nameHeading = document.createElement('th');
+    nameHeading.classList.add('heading');
+    nameHeading.textContent = "Task Name";
+    headRow.appendChild(nameHeading);
+
+    const descHeading = document.createElement('th');
+    descHeading.classList.add('heading');   
+    descHeading.textContent = "Description";
+    headRow.appendChild(descHeading);
+
+    const priorityHeading = document.createElement('th');
+    priorityHeading.classList.add('heading');
+    priorityHeading.textContent = "Priority";
+    headRow.appendChild(priorityHeading);
+
+    const projHeading = document.createElement('th');
+    projHeading.classList.add('heading');
+    projHeading.textContent = "Associated Project";
+    headRow.appendChild(projHeading);
+
+    const deleteHeading = document.createElement('th');
+    headRow.appendChild(deleteHeading);
+
+    const tableBody = document.createElement('tbody');
+    tasksTable.appendChild(tableBody);
+
+    for (let i = 0; i < a.length; i++) {        
+        let newRow = document.createElement('tr');
+        tableBody.appendChild(newRow);
+
+        let markCompletedCell = document.createElement('td');
+        newRow.appendChild(markCompletedCell);
+
+        let markCompletedInput = document.createElement('input');
+        markCompletedInput.setAttribute('type', 'checkbox');
+        markCompletedCell.appendChild(markCompletedInput);
+
+        let dateCell = document.createElement('td');
+        dateCell.textContent = a[i].dueDate;
+        newRow.appendChild(dateCell);
+
+        let nameCell = document.createElement('td');
+        nameCell.textContent = a[i].name;
+        newRow.appendChild(nameCell);
+
+        let descCell = document.createElement('td');
+        descCell.textContent = a[i].desc;
+        newRow.appendChild(descCell);
+
+        let priorityCell = document.createElement('td');
+        newRow.appendChild(priorityCell);
+
+        let priorityCellIcon = document.createElement('img');
+        priorityCellIcon.classList.add('priority-icon');
+        let priorityValue = a[i].priority;
+
+        if (priorityValue === "high") {
+            priorityCellIcon.src = highPriorityImage;
+            priorityCellIcon.alt = "high priority";
+        } else if (priorityValue === "medium") {
+            priorityCellIcon.src = medPriorityImage;
+            priorityCellIcon.alt = "medium priority";
+        } else if (priorityValue === "low") {
+            priorityCellIcon.src = lowPriorityImage;
+            priorityCellIcon.alt = "low priority";
+        }
+        priorityCell.appendChild(priorityCellIcon);
+        
+
+        let projCell = document.createElement('td');
+        projCell.textContent = a[i].assocProj;
+        newRow.appendChild(projCell);
+
+        let deleteIconCell = document.createElement('td');
+        newRow.appendChild(deleteIconCell);
+
+        let deleteIcon = document.createElement('img');
+        deleteIcon.classList.add('delete-task')
+        deleteIcon.src = trashBinImage;
+        deleteIcon.alt = "Remove task";
+        deleteIconCell.appendChild(deleteIcon);
+    }  
+};
+
+export function removeTaskTable() {
+    const taskTable = document.getElementById('new-task-container');
+    taskTable.remove();
+    
+}
+
+
 
 
 
